e3d8383065aae2c990d1779d643b72930fd45cc9
angellyapaza2809-afk <angellyapaza2809@gmail.com>
2026-08-06 23:18:09 -0500

    Update brand mark to use logo image

--- js/nav.js
+++ js/nav.js
@@ -33,7 +33,7 @@ function renderShell() {
   shell.innerHTML = `
     <aside class="sidebar">
       <div class="brand">
-        <div class="brand-mark">SF</div>
+        <div class="brand-mark"><img src="assets/logo-mark.png" alt="San Fernando" /></div>
         <div class="brand-text">
           <strong>Ciclo Térmico BB</strong>
           <span>San Fernando · MVP</span>
